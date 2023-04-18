cb11aae809771c87f2a839f9ae9539427b906899
Eric Pyle <epyle@biblesocieties.org>
2023-04-17 19:02:06 -0500

    hindsight bug capture

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import '@fortawesome/fontawesome-free/css/all.min.css'
 
